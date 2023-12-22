6d975c7fa271386594c5f1cff2e8f1f16a81a4f2
samandarmaxsutov <gitsamandar1@gmail.com>
2023-12-22 13:19:25 +0500

    finished

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,47 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+
+                String[] parts = line.split(": ", 2);
+                if (parts.length == 2) {
+                    String key = parts[0].trim();
+                    String value = parts[1].trim();
+
+                    switch (key) {
+                        case "Name":
+                            profile.setName(value);
+                            break;
+                        case "Age":
+                            profile.setAge(Integer.parseInt(value));
+                            break;
+                        case "Email":
+                            profile.setEmail(value);
+                            break;
+                        case "Phone":
+                            profile.setPhone(Long.parseLong(value));
+                            break;
+
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        return profile;
     }
+
+
 }
